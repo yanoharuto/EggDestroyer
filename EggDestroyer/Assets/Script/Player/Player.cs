c700cdb1cd7eb4572444b9efa543f09ba3fee45e
yanoharuto <8bitharuto@gmail.com>
2022-04-07 11:29:57 +0900

    とりあえず上昇

---
+++ EggDestroyer/Assets/Script/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Range(1.0f, 3.0f)] private float mRiseVel;
+    private float mRiseA = 0;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Vector3 RiseVel = new Vector3(0, mRiseA + mRiseA, 0);
+            transform.position += RiseVel;
+            mRiseA += 0.1f;
+        }
+    }
+}
---
+++ EggDestroyer/Assets/Script/Player/PlayerStateEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateEnum : MonoBehaviour
+{
+    enum PlayerState
+    {
+        Idol,//待機
+        rise,//上昇
+        Descent//下降
+    };
+}
